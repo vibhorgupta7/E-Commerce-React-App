fc1ef8d236a858bc812799579b8c812dbc677dbc
vibhorgupta7 <vibhor4k@gmail.com>
2021-03-22 23:25:35 +0530

    components & pages folder added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // npm install node-sass
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
