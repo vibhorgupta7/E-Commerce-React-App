e0b1ddf7251ebb6993a480c0ab152afe21eebb8c
vibhorgupta7 <vibhor4k@gmail.com>
2021-03-24 14:44:58 +0530

    Shop Page added

--- src/App.js
+++ src/App.js
@@ -3,23 +3,15 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => {
-  return(
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  )
-  
-}
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>            {/* whenever a route is being made, the component thta is called is sent 3 parameters: Location, History, Match */}
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
