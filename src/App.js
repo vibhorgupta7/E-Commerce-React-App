c39dd7b386df3e865f9655a67b99dbd4d0396465
vibhorgupta7 <vibhor4k@gmail.com>
2021-03-24 16:23:19 +0530

    Header added & completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 // npm install node-sass
 import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component';
 
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+  
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>            {/* whenever a route is being made, the component thta is called is sent 3 parameters: Location, History, Match */}
         <Route path='/shop' component={ShopPage} />
